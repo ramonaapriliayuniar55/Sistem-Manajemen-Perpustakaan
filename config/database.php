b2b753bb45aca136c78009533f7a1e16d97a41ab
Ramona <ramona.aprilia.yuniar24039@mhs.uingusdur.ac.id>
2026-05-04 12:36:51 +0700

    inisialisasi projek

--- config/database.php
+++ config/database.php
@@ -8,7 +8,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'perpustakaan');
+define('DB_NAME', 'perpustakaanp');
  
 // Buat koneksi
 $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
